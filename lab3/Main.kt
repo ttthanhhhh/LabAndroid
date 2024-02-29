6ff5cbfd689e1d3784e6f1ad4945ee5a6e54b7ed
_tha.znh_ <135533372+ttthanhhhh@users.noreply.github.com>
2024-02-29 20:29:42 +0700

    Add files via upload

---
+++ lab3/Main.kt
@@ -0,0 +1,10 @@
+package com.example.lab3
+
+import com.example.lab3.myapp.Aquarium
+fun main() {
+    buildAquarium()
+}
+fun buildAquarium() {
+    val myAquarium = Aquarium()
+    myAquarium.printSize()
+}
